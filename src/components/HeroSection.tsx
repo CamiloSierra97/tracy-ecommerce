339630c890313841ed37699a6e4d4daedc62fb6c
Juan Camilo Sierra Rojas <camilo.sierra@co.onelink.bpo>
2025-12-05 08:52:40 -0500

    Hero modificado first mobile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,7 +9,7 @@ const HeroSection: React.FC = () => {
   return (
     <section className="hero w-full h-full md:h-1/12 flex flex-col md:flex-row justify-center">
       {/* <aside> para contenido tangencial: la imagen de fondo */}
-      <figure className="hero__image-container w-full h-[90vh] md:w-1/3 md:h-screen lg:w-3/12 relative bg-burgundy">
+      <figure className="hero__image-container w-full h-[90vh] md:w-1/3 md:h-screen lg:w-1/3 lg:h-[90vh] relative bg-burgundy">
         <Image
           src="/Background1.svg"
           alt="Fondo de lencería"
@@ -23,7 +23,7 @@ const HeroSection: React.FC = () => {
         />
       </figure>
       {/* <article> para el contenido autocontenido (el mensaje y el CTA) */}
-      <article className="hero__content w-full md:w-1/2 pb-8 pt-4 px-6 md:pt-6 md:px-8 z-10 bg-burgundy-light flex flex-col justify-center items-center text-center relative">
+      <article className="hero__content w-full md:w-1/2 lg:w-2/5 pb-8 pt-4 px-6 md:pt-6 md:px-8 z-10 bg-burgundy-light flex flex-col justify-center items-center text-center relative">
         <div
           className={`hero__title inline-block ${roboto_serif.className} text-5xl leading-20 md:text-4xl text-light-gold mx-0 my-2`}
         >
