db92bfb57f5f715809f68b67d1d2ef09fb686706
SierraDev <sierracamilo3@gmail.com>
2025-12-19 17:40:26 -0500

    Mejora de performance Server-Side Hydration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
 import Products from "@/components/product/Products";
 import { metadata } from "./layout";
 import { roboto_serif } from "@/lib/fonts";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import HeroSection from "@/components/layout/HeroSection";
 import Image from "next/image";
+import WooCommerceService from "@/services/WooCommerceService";
 
-export default function Page() {
+export default async function Page() {
+  const queryClient = new QueryClient();
+
+  // Prefetch data on the server
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["products"],
+    queryFn: async ({ pageParam = 1 }) => {
+      // Direct server-side call to WooCommerce
+      return await WooCommerceService.getProducts({
+        page: pageParam as number,
+        per_page: 12,
+      });
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage: any, allPages: any) => {
+       const currentTotalPages = lastPage.totalPages;
+       const nextPage = allPages.length + 1;
+       return nextPage <= currentTotalPages ? nextPage : undefined;
+    }
+  });
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <main className="main">
         <section
           className={`hero-section flex ${roboto_serif.className} justify-center relative overflow-hidden `}
@@ -27,6 +49,6 @@ export default function Page() {
 
         </section>
       </main>
-    </>
+    </HydrationBoundary>
   );
 }
