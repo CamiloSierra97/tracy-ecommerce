a3be878ec825513d95efa27d9865ce5f26ec635c
SierraDev <sierracamilo3@gmail.com>
2026-01-10 09:09:48 -0500

    Se elimina stopPropagation

--- src/components/product/ProductsGrid.tsx
+++ src/components/product/ProductsGrid.tsx
@@ -345,7 +345,6 @@ function ProductCard({
             <button
               onClick={(e) => {
                 e.preventDefault();
-                e.stopPropagation();
                 addToCart(product);
               }}
               className="product-card__btn-icon pointer-events-auto glassmorphism text-black p-3 size-10 rounded-full lg:hover:bg-ivory lg:hover:text-burgundy transition-all flex justify-center items-center shadow-sm bg-ivory/80"
